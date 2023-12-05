6d419b7878f693e2b7fe89b1683fe6e180809d0b
Jin Su Park <jinsupark@icloud.com>
2023-12-05 04:49:44 -0500

    Add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
-import alpinejs from '@astrojs/alpinejs';
-
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
--- tailwind.rinsedocs.config.js
+++ tailwind.rinsedocs.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		container: {
 			center: true,
 		},
 		extend: {
 			fontFamily: {
-				sans: ['"Inter var", sans-serif'],
+				sans: ['InterVariable, sans-serif'],
 			},
 		},
 	},
