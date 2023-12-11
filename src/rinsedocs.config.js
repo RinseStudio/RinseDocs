d2cf3f7f8df95e93b6f48fcac952599bd04c8177
Jin Su Park <jinsupark@icloud.com>
2023-12-11 16:36:36 -0500

    Add

--- src/rinsedocs.config.js
+++ src/rinsedocs.config.js
@@ -1,27 +1,27 @@
 // Define your SITE constant without using 'as const'
 export const SITE = {
-	website: 'https://astro-paper.pages.dev/',
-	author: 'Sat Naing',
-	desc: 'A minimal, responsive and SEO-friendly Astro blog theme.',
-	title: 'AstroPaper',
-	ogImage: 'astropaper-og.jpg',
-	lightAndDarkMode: true,
-	postPerPage: 3,
+  website: 'https://astro-paper.pages.dev/',
+  author: 'Sat Naing',
+  desc: 'A minimal, responsive and SEO-friendly Astro blog theme.',
+  title: 'AstroPaper',
+  ogImage: 'astropaper-og.jpg',
+  lightAndDarkMode: true,
+  postPerPage: 3,
 
-	sidebarOverrides: {
-		components: [
-			{
-				label: ['elements', 'Elements'],
-			},
-			{
-				label: ['modules', 'Modules'],
-			},
-			{
-				label: ['layouts', 'Layouts'],
-			},
-			{
-				label: ['templates', 'Templates'],
-			},
-		], // Use the type for the array of overrides here
-	},
+  sidebarOverrides: {
+    components: [
+      {
+        label: ['elements', 'Elements'],
+      },
+      {
+        label: ['modules', 'Modules'],
+      },
+      {
+        label: ['templates', 'Templates'],
+      },
+      {
+        label: ['layouts', 'Layouts'],
+      },
+    ], // Use the type for the array of overrides here
+  },
 };
