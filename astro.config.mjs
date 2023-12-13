c2f6e53c3118a37e23f778bbc46b6a7ec82bdbdf
Jin Su Park <jinsupark@icloud.com>
2023-12-13 14:19:50 -0500

    Add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,18 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		mdx(),
-	],
-	markdown: {
-		shikiConfig: {
-			// Choose from Shiki's built-in themes (or add your own)
-			// https://github.com/shikijs/shiki/blob/main/docs/themes.md
-			wrap: true,
-			experimentalThemes: {
-				light: 'github-light',
-				dark: 'github-dark',
-			},
-		},
-	},
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+  ],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      wrap: true,
+      theme: 'github-dark',
+    },
+  },
 });
---
+++ src/components/rinseui/modules/Dropdown/index.ts
@@ -0,0 +1,14 @@
+import Dropdown from '@rinseui/modules/Dropdown/Dropdown.astro';
+import Group from '@rinseui/modules/Dropdown/Group.astro';
+import Header from '@rinseui/modules/Dropdown/Header.astro';
+import Item from '@rinseui/modules/Dropdown/Item.astro';
+import Items from '@rinseui/modules/Dropdown/Item.astro';
+
+const ExtendedDropdown = Object.assign(Dropdown, {
+  Group,
+  Header,
+  Item,
+  Items,
+});
+
+export default ExtendedDropdown;
