7b4f3bbbe3b58688e1a255abea690cd6da666019
Jin Su Park <jinsupark@icloud.com>
2024-01-08 15:38:05 -0500

    Add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx(), alpinejs()],
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       wrap: true,
-      theme: 'github-dark',
-    },
-  },
+      theme: 'github-dark'
+    }
+  }
 });
