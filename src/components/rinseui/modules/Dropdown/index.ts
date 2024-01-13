7ac6d038dffa4b8ec14cc73343b6cb0bea66628d
Jin Su Park <jinsupark@icloud.com>
2024-01-12 22:48:11 -0500

    Update index.ts

--- src/components/rinseui/modules/Dropdown/index.ts
+++ src/components/rinseui/modules/Dropdown/index.ts
@@ -1,11 +1,11 @@
-import Dropdown from '@rinseui/modules/Dropdown/Dropdown.astro';
-import Trigger from '@rinseui/modules/Dropdown/Trigger.astro';
-import Content from '@rinseui/modules/Dropdown/Content.astro';
-import Group from '@rinseui/modules/Dropdown/Group.astro';
-import Header from '@rinseui/modules/Dropdown/Header.astro';
-import Item from '@rinseui/modules/Dropdown/Item.astro';
-import Items from '@rinseui/modules/Dropdown/Items.astro';
-import Separator from '@rinseui/modules/Dropdown/Separator.astro';
+import Dropdown from './Dropdown.astro';
+import Trigger from './Trigger.astro';
+import Content from './Content.astro';
+import Group from './Group.astro';
+import Header from './Header.astro';
+import Item from './Item.astro';
+import Items from './Items.astro';
+import Separator from './Separator.astro';
 
 const Component = Object.assign(Dropdown, {
   Group,
