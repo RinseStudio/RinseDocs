0cabfbd0116a31399dd66d36675c4c397781d839
Jin Su Park <jinsupark@icloud.com>
2024-01-22 03:17:26 -0500

    Add

--- src/components/ui/elements/Card/index.ts
+++ src/components/ui/elements/Card/index.ts
@@ -1,6 +1,6 @@
-import Card from '@rinseui/elements/Card/Card.astro';
-import Group from '@rinseui/elements/Card/Group.astro';
-import Separator from '@rinseui/elements/Card/Separator.astro';
+import Card from './Card.astro';
+import Group from './Group.astro';
+import Separator from './Separator.astro';
 
 const Component = Object.assign(Card, {
   Group,
