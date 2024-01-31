a11b948196bfb3836a4c1b2f1fce8de8fb2016bf
Jin Su Park <jinsupark@icloud.com>
2024-01-31 11:18:16 -0500

    Add

--- src/components/ui/modules/Dropdown/index.ts
+++ src/components/ui/modules/Dropdown/index.ts
@@ -1,20 +1,19 @@
 import Dropdown from './Dropdown.astro';
-import Trigger from './Trigger.astro';
-import Content from './Content.astro';
-import Group from './Group.astro';
-import Header from './Header.astro';
-import Item from './Item.astro';
-import Items from './Items.astro';
-import Separator from './Separator.astro';
+import DropdownTrigger from './DropdownTrigger.astro';
+import DropdownContent from './DropdownContent.astro';
+import DropdownGroup from './DropdownGroup.astro';
+import DropdownHeader from './DropdownHeader.astro';
+import DropdownItem from './DropdownItem.astro';
+import DropdownList from './DropdownList.astro';
+import DropdownSeparator from './DropdownSeparator.astro';
 
-const Component = Object.assign(Dropdown, {
-  Group,
-  Trigger,
-  Content,
-  Header,
-  Item,
-  Items,
-  Separator,
-});
-
-export default Component;
+export {
+  Dropdown,
+  DropdownGroup,
+  DropdownTrigger,
+  DropdownContent,
+  DropdownHeader,
+  DropdownItem,
+  DropdownList,
+  DropdownSeparator,
+};
