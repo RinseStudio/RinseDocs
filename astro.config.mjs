be90865e1968416d517a2bff9d98af8bcd019221
Jin Su Park <jinsupark@icloud.com>
2024-02-08 10:48:19 -0500

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-
 import icon from 'astro-icon';
+
+import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
+    }),
+    expressiveCode({
+      themes: ['vitesse-light', 'vitesse-dark'],
     }),
     mdx(),
     icon(),
