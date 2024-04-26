142529f88ca68dc25b7e45352513020c2a72e077
Jin Su Park <jinsupark@icloud.com>
2024-04-26 11:08:49 -0400

    Add

--- src/components/Tooltip/index.ts
+++ src/components/Tooltip/index.ts
@@ -1,5 +1,5 @@
 import Tooltip from './Tooltip.astro';
-import TooltipTrigger from './Trigger.astro';
-import TooltipContent from './Content.astro';
+import TooltipTrigger from './TooltipTrigger.astro';
+import TooltipContent from './TooltipContent.astro';
 
 export { Tooltip, TooltipTrigger, TooltipContent };
